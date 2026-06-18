ef84ce535ab53e95b4545f425d28e82c84152756
Syed Hamza <syedhamzase12@gmail.com>
2026-06-18 17:57:16 +0500

    Driver API added

--- truck_Cargo/apps/urls.py
+++ truck_Cargo/apps/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path , include  
-from apps.users.Driver import RequestOTPView , VerifyOTPView , DriverProfileView
+from apps.users.Driver import Driver_trip_history, RequestOTPView , VerifyOTPView , DriverProfileView , Driver_actve_trip, TripInfo
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,10 +10,12 @@ urlpatterns = [
 path('Driver-request-otp/',RequestOTPView,name="request_otp"),
 path(    "verify-otp/",    VerifyOTPView , name="verify_otp"),
 path('Driver-profile/', DriverProfileView.as_view(), name="driver_profile"),
-
+path('driver-trip-history/', Driver_trip_history.as_view(), name="driver_trip_history"),
+path('driver-active-trip/', Driver_actve_trip.as_view(), name="driver_active_trip"),
+path('trip-info/<int:trip_id>/', TripInfo.as_view(), name="trip_info"),
 # Endpoint to login and get tokens
 path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    
+
     # Endpoint to get a new access token using a refresh token
 path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- truck_Cargo/apps/users/Driver.py
+++ truck_Cargo/apps/users/Driver.py
@@ -1,9 +1,10 @@
+from pyclbr import Class
 import random
 
 # from rest_framework.views import APIView
 from django.http import JsonResponse
 import json
-from apps.models import User
+from apps.models import Driver, Trip, User
 from apps.utils.redis import redis_client
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -111,10 +112,83 @@ class DriverProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-
+        user_id = request.user.id
+        Driver_object = Driver.objects.filter(user_id=user_id).first()
         return JsonResponse({
+            "User_Role" : "Driver",
+            "username":request.user.username,
+            "phone":request.user.phone,
+            "license_number":Driver_object.license_number,
+            "employee_id":Driver_object.employee_id,
+        })
+    
 
-            "username": request.user.username
+class Driver_trip_history(APIView):
 
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        Trip_object = Trip.objects.filter(driver__user_id=request.user.id)
+        print(Trip_object.first())
+        
+        trip = Trip_object.values(
+            "id",
+            "load__origin",
+            "load__destination",
+            "status",
+            "start_datetime"
+        )
+        
+        return JsonResponse({
+            "trip_history": list(trip)
         })
 
+
+class Driver_actve_trip(APIView):
+   
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        Trip_object = Trip.objects.filter(driver__user_id=request.user.id, status="Planned")
+        trip = Trip_object.values(
+            "id",
+            "load__origin",
+            "load__destination",
+            "status",
+            "start_datetime"
+        )
+        return JsonResponse({
+            "trip_history": list(trip)
+        })
+
+class TripInfo(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, trip_id):
+        trip = Trip.objects.select_related("load", "truck").filter(
+            driver__user_id=request.user.id,
+            id=trip_id
+        ).first()
+
+        if not trip:
+            return JsonResponse({"error": "Trip not found"}, status=404)
+
+        # latest truck location
+        truck_location = trip.truck.locations.order_by("-recorded_at").first()
+
+        trip_data = {
+            "load_number": trip.load.load_number,
+            "load_origin": trip.load.origin,
+            "load_destination": trip.load.destination,
+            "load_weight": trip.load.weight_lbs,
+            "load_pickup_contact_phone": trip.load.pickup_contact_phone,
+            "load_delivery_contact_phone": trip.load.delivery_contact_phone,
+            "truck_latitude": truck_location.latitude if truck_location else None,
+            "truck_longitude": truck_location.longitude if truck_location else None,
+            "truck_number_plate": trip.truck.license_plate,
+            "status": trip.status,
+            "start_datetime": trip.start_datetime,
+            "end_datetime": trip.end_datetime,
+            "trip_assigned_by": trip.assigned_by.username if trip.assigned_by else None
+        }
+
+        return JsonResponse({"trip_info": trip_data})
